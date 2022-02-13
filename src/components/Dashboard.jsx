3027527f1603963bd41842a9f136aef4b821601b
chinnarasu-kanthan <crasu341@gmail.com>
2022-02-13 16:17:01 +0530

    1 file added, 1 deleted

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { lazy, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const importView = (layout) =>
+  React.lazy(() => import(`../components/`+layout+`.jsx`).catch((e) => console.log(e)));
+
+const Dashboard = () => {
+  const [views, setViews] = useState([]);
+  const { user: currentUser } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    console.log(currentUser.layout);
+    async function loadViews() {
+      const View = importView(currentUser.layout);
+      setViews(<View />);
+    }
+
+   
+
+   loadViews();
+  }, [currentUser]);
+
+  return (
+    <React.Suspense fallback="Loading views...">
+      <div className="container">{views}</div>
+    </React.Suspense>
+  );
+};
+
+export default Dashboard;
